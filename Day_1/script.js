1a89e47ea72b691cd10b2093b6578a24bc7f3fc4
Aastha <aasthamishra175@gmail.com>
2024-03-09 20:18:13 +0545

    first day trying js

---
+++ Day_1/script.js
@@ -0,0 +1,26 @@
+
+// alert("Hi there, welcome to my channel.");
+
+console.log("Hey There!!");
+console.log("This is amazing.");
+
+
+var a = prompt("Enter the number: ");
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer.");
+
+if (isTrue){
+    console.log("Your PC is about to blast.");
+}
+else{
+    console.log("Your PC is not about to blast");
+}
+
+prompt("This is another prompt");
+
+
+
+
+    console.log("Your number is : " + a);
+
+    document.title = "Nope";
+    
