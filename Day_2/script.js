59b600e5d58c103c9615e80822af22d444f4d3cb
Aastha <aasthamishra175@gmail.com>
2024-03-11 22:12:19 +0545

    Javascript to calculate the factorial.

--- Day_2/script.js
+++ Day_2/script.js
@@ -1,1 +1,11 @@
 console.log("Helooo World");
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  console.log(factorial(5)); // Output: 120
