a38dc5b4ca82e97371365b29f90518180eedb67d
Aastha <aasthamishra175@gmail.com>
2024-08-04 19:43:38 +0545

    Task 2 for arithmetic operator.

--- D2_challange/script.js
+++ D2_challange/script.js
@@ -12,3 +12,10 @@ console.log(c);
 let x = 6 - 8;
 document.getElementById('demo').innerHTML = x;
 window.alert("Welcome and have a glare.");
+
+//Task 2: Write a program to subtract two numbers and log the result to the console.
+
+let y = 88;
+let z = 99;
+const s = z - x;
+console.log(s);
