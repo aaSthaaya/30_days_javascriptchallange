51aa4878f9e25983c51fa765207a52cd79f7b391
Aastha <aasthamishra175@gmail.com>
2024-08-04 21:15:37 +0545

    Activity 2 - Assignment Operator

--- D2_challange/script.js
+++ D2_challange/script.js
@@ -8,7 +8,7 @@ let a = 2;
 let b = 3;
 const c = a + b;
 console.log(c);
-
+//
 let x = 6 - 8;
 document.getElementById('demo').innerHTML = x;
 window.alert("Welcome and have a glare.");
@@ -19,3 +19,42 @@ let y = 88;
 let z = 99;
 const s = z - x;
 console.log(s);
+
+//Task 3: Write a program to multiply three numbers and log results to the console.
+
+let num1 = 44;
+let num2 = 33;
+let num3 = 66;
+const mul = num3 * num2 * num1;
+console.log(mul);
+
+//Task 4: Write a program to divide two numbers and log results to the console.
+
+let num4 = 17;
+let num5 = 34;
+const div = num5 / num4;
+console.log(div);
+
+//Task 5: Write a program to find the remainder when one number is divide by other and log the result to console.
+
+let i = 55;
+let j = 7;
+const k = 55 % 7;
+console.log(k);
+
+//Activity 2: Assignment operators
+
+//Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let v = 99;
+v += 1;
+console.log(v);
+
+//Task 7: Use the -= operator to subtract to a variable and log the result to the console.
+
+let w = 738345;
+w -= 732542;
+console.log(w);
+
+
+
