39b5771a5c3246fbda1d25f4643d1020ee4f1f51
Aastha <aasthamishra175@gmail.com>
2024-08-05 22:12:18 +0545

    If/Else statement of control structure.

--- D3_challange/script.js
+++ D3_challange/script.js
@@ -1,2 +1,26 @@
 console.log("Day 3: Welcome once again developer Aastha Mishra...Let's Do This!")
+console.log("Control Structure");
 
+//Activity 1: If/Else Statement
+
+//Task1: Write a program to check if a number is positive, negative or zero and log the result to the console.
+
+num = 0;
+
+if (num > 0) {
+    console.log("The number is positive.")
+} else if (num < 0) {
+    console.log("The number is negative.")
+} else {
+    console.log("The number is zero.");
+}
+
+//Task2: Write a program to check if a person is eligible to vote or not(age>=18) and console the output to the log.
+
+personAge = 12
+
+if (personAge >= 18) {
+    console.log("They can vote.");
+} else {
+    console.log("They cannot vote.");
+}
